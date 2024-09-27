44bf3820fa4f70130f6bd522a6ee669c32192e43
fhosseiny <fhosseiny267@gmail.com>
2024-09-27 17:54:08 +0200

    add contact component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Contact from "./components/contact";
+import ContactsList from "./components/contactsList";
+
+function App() {
+ return (
+    <>
+      <Header />
+      <Contact />
+      <ContactsList />
+      </>
+  );
+}
+export default App;
---
+++ src/components/contactsList.jsx
@@ -0,0 +1,9 @@
+
+function ContactsList(props) {
+  console.log(props)
+  return (
+    <div>Contact List</div>
+  )
+}
+
+export default ContactsList
